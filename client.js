3290c87d7212646114e1c3fd7adac1a35e230159
Elshan Magsudov <elio@jyanik.com>
2022-11-17 14:11:39 -0500

    moved connect function into a separate file. implemented export.modules and required it in play.js. added printing message to confirm connection and send my snake's name to the server to be displayed in the game board

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // change to IP address of computer, more on that below
+    port: 50541,
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: XYZ");
+  });
+
+  // conn.on("connect", () => {
+  //   conn.write("Name: XYZ");
+  // });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf-8");
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const net = require("net");
+const { connect } = require("./client");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // change to IP address of computer, more on that below
-    port: 50541,
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf-8");
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
